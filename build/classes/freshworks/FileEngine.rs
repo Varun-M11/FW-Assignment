0d89625a79006089207a39c661032351714e839b
Varun Mishra <mishrava@rknec.edu>
2021-01-01 16:43:39 +0530

    Add files via upload

---
+++ build/classes/freshworks/FileEngine.rs
@@ -0,0 +1,6 @@
+freshworks.TimeExceeded
+freshworks.InvalidKey
+freshworks.KeySizeExceeded
+freshworks.FileEngine
+freshworks.DuplicateKey
+freshworks.ValueSizeExceeded
---
+++ src/freshworks/Assignment.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freshworks;
+
+import java.util.Scanner;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author VarunM
+ */
+public class Assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        FileEngine fe = new FileEngine();
+        
+       /* 
+        
+        You can call all the methods from FileEngine class which contains all the backend
+        functionalities. As it was mentioned that the code will be unit tested that;s why 
+        function call is not implemented here.
+        
+        But still you can runn this code and see that the json file with blank Key-Value will be created.
+        
+        Thank You !     
+        
+        
+       */
+      }
+    }
+    
+
